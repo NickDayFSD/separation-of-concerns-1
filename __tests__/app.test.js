81c4e3dac308e4e9bdafcf05f9fa1af9ee4b537e
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-06-10 17:10:07 -0700

    Test now operational

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,16 @@
+require('dotenv').config();
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-jest.mock('twilio', () => () => ({
-  messages: {
-    create: jest.fn(),
-  },
-}));
+// jest.mock('twilio', () => () => ({
+//   messages: {
+//     create: jest.fn(),
+//   },
+// }));
 
-describe('03_separation-of-concerns-demo routes', () => {
+describe('03_separation-of-concerns routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
