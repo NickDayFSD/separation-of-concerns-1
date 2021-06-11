979827edfe46c84559f5a45f70baae6479581c2a
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-06-11 13:56:48 -0700

    test written for findById

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Candy = require('../lib/models/Candy');
 
 describe('03_separation-of-concerns routes', () => {
   beforeEach(() => {
@@ -42,4 +43,18 @@ describe('Candy routes', () => {
 
     expect(res.body).toEqual(expectation);
   });
+
+  it('retrieves a specific candy from our database', async () => {
+    const candy = await Candy.insert({
+      name: 'Laffy Taffy',
+      texture: 'chewy',
+      size: 'small'
+    });
+
+    const res = await request(app)
+      .get(`/api/v1/candies/${candy.id}`);
+
+    expect(res.body).toEqual(candy);
+  });
+
 });
