046a67ef786b3a39ae0457cfbdfb51ea02f5d5f9
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-06-10 17:58:37 -0700

    OG test passes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,26 +4,41 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-// jest.mock('twilio', () => () => ({
-//   messages: {
-//     create: jest.fn(),
-//   },
-// }));
-
 describe('03_separation-of-concerns routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  it('creates a new order in our database and sends a text message', async () => {
+  it('creates a new order in our database', async () => {
     const expectation = {
       id: '1',
-      quantity: 10,
+      quantityOfItems: 10,
     };
 
     const res = await request(app)
       .post('/api/v1/orders')
       .send({ quantity: 10 });
+
+    expect(res.body).toEqual(expectation);
+  });
+});
+
+describe('First routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('creates a new candy in our database', async () => {
+    const expectation = {
+      id: '1',
+      name: 'Big Hunk',
+      texture: 'chewy',
+      size: 'large'
+    };
+
+    const res = await request(app)
+      .post('/api/v1/candies')
+      .send({ name: 'Big Hunk', texture: 'chewy', size: 'large' });
 
     expect(res.body).toEqual(expectation);
   });
